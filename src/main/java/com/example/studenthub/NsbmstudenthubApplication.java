e623fc393013b25156777b14a5eaa121ae50b251
kaveeshakalanka <kaveeshakalanka0129@gmail.com>
2026-02-09 20:09:52 +0530

    initial project structure

---
+++ src/main/java/com/example/studenthub/NsbmstudenthubApplication.java
@@ -0,0 +1,13 @@
+package com.example.studenthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsbmstudenthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsbmstudenthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studenthub/NsbmstudenthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studenthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsbmstudenthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
